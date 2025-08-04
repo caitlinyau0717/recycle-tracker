c34b1470d6e1c277df2fb3afe6c2891050770844
caitlinyau0717 <yauc2@rpi.edu>
2025-08-04 17:51:03 -0400

    fixed navigation on camera and profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -27,7 +27,13 @@ class _ProfilePageState extends State<ProfilePage> {
           title: Row(
             children: [
               GestureDetector(
-                onTap: () => Navigator.pop(context),
+                onTap: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                    (Route<dynamic> route) => false,
+                  );
+                },
                 child: const Row(
                   children: [
                     Icon(Icons.arrow_back_ios, color: Colors.black54, size: 16),
