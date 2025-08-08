3cc6ca9d67310baaea8985b7daf43a0578d74bf5
TmgEE <etmg02@hotmail.com>
2025-08-08 18:07:26 -0400

    testing

--- lib/pages/quantity.dart
+++ lib/pages/quantity.dart
@@ -3,54 +3,72 @@ import 'package:mongo_dart/mongo_dart.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 
 void main(List<String> arguments) async {
-  OpenFoodAPIConfiguration.userAgent = UserAgent(name: 'Your app name', url: 'Your url, if applicable');
+  OpenFoodAPIConfiguration.userAgent =
+      UserAgent(name: 'Your app name', url: 'Your url, if applicable');
+
   ProductQueryConfiguration config = ProductQueryConfiguration(
     '5000112654523',
     version: ProductQueryVersion.v3,
     language: OpenFoodFactsLanguage.ENGLISH,
   );
+
   ProductResultV3 product = await OpenFoodAPIClient.getProductV3(config);
-  //print('Hello world: ${food_facts_project.calculate()}!');
-  print("Hi. packaging\n");
-  print(product.product?.packaging); // Coca Cola Zero
-  List<String> here = product.product?.categoriesTags??[];
-  print("categories\n");
-  for (String tag in here){
+
+  // Packaging
+  print("packaging");
+  print(product.product?.packaging);
+
+  // Categories
+  List<String> here = product.product?.categoriesTags ?? [];
+  print("categories");
+  for (String tag in here) {
     tag = tag.toLowerCase();
-    if(tag.contains("beverage")){
+    if (tag.contains("beverage")) {
       print("ok");
     }
   }
-  //Quantity
-  //
-  String quantity = product.product?.quantity??"";
+
+  // Quantity parsing
+  String quantity = product.product?.quantity ?? "";
   quantity = quantity.toLowerCase();
   String quantityMeasured = "";
-  int milliletersFound = quantity.indexOf("ml");
-  if (milliletersFound == -1){
-    print("I DID NOT FIND MILLILITERS. NOW I WILL ENTER IF STATEMENTS");
-    milliletersFound = quantity.indexOf("fl");
-    if (milliletersFound == -1){
-      milliletersFound = quantity.indexOf("l");
-      quantityMeasured = "l";
-      if (milliletersFound == -1){
-        milliletersFound = 2;
-      }
-      }
-      else{
-      quantityMeasured = "l"; 
+
+  // Check for ml
+  int mlFound = quantity.indexOf("ml");
+  if (mlFound != -1) {
+    quantityMeasured = "ml";
+  } else {
+    print("I DID NOT FIND ML");
+
+    // Check for fl
+    int flFound = quantity.indexOf("fl");
+    if (flFound != -1) {
+      print("I DID NOT FIND FL");
+
+      quantityMeasured = "fl";
+      mlFound = flFound; // reuse position for substring extraction
+    } else {
+      // Check for l
+      int lFound = quantity.indexOf("l");
+      if (lFound != -1) {
+        print("I DID NOT FIND L");
+
+        quantityMeasured = "l";
+        mlFound = lFound;
+      } else {
+        // Fallback position
+        mlFound = 2;
       }
     }
-  else{
-      quantityMeasured = "ml"; 
   }
-  quantity = quantity.substring(0,milliletersFound);
+
+  // Extract numeric part
+  quantity = quantity.substring(0, mlFound);
   quantity = quantity.replaceAll(" ", "");
   quantity = quantity.replaceAll(",", ".");
   double doubleQuantity = double.parse(quantity);
-  print(milliletersFound);
+
+  print(mlFound);
   print(quantityMeasured);
   print(doubleQuantity);
-  //quantity = quantity.substring(0,fluidFound);
-  //print(quantity);
-} 
+}
